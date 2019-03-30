0ec2018e6ae589290ce1fd8a98c264668c8b0385
Vova Klymenko <klymenkowolodymyr@gmail.com>
2019-03-30 17:55:46 +0200

    Fix date time issues

--- Clinic.Data/Automapper/NotificationProfile.cs
+++ Clinic.Data/Automapper/NotificationProfile.cs
@@ -13,14 +13,17 @@ namespace Clinic.Data.Automapper
                     nDto => nDto.UserName,
                     options => options.MapFrom(n => $"{n.User.Name} {n.User.Surname}"))
                 .ForMember(nDto => nDto.UserMail, options => options.MapFrom(n => n.User.Email))
-                .ForMember(nDto => nDto.UserId, options => options.MapFrom(n => n.User.Id));
+                .ForMember(nDto => nDto.UserId, options => options.MapFrom(n => n.User.Id))
+                .ForMember(nDto => nDto.CreatedDate, options => options.MapFrom(n => n.CreationDate));
             CreateMap<NotificationDto, Notification>()
                 .ForMember(n => n.User, options => options.Ignore())
-                .ForMember(n => n.UserId, options => options.Ignore());
+                .ForMember(n => n.UserId, options => options.Ignore())
+                .ForMember(n => n.CreationDate, options => options.MapFrom(nDto => nDto.CreatedDate));
 
             CreateMap<CreateNotificationDto, Notification>()
                 .ForMember(n => n.Author, options => options.Ignore())
-                .ForMember(n => n.User, options => options.Ignore());
+                .ForMember(n => n.User, options => options.Ignore())
+                .ForMember(n => n.CreationDate, options => options.MapFrom(nDto => nDto.CreationDate));
         }
     }
 }
--- ClinicApi/Automapper/NotificationProfile.cs
+++ ClinicApi/Automapper/NotificationProfile.cs
@@ -2,7 +2,6 @@
 using Clinic.Core.DtoModels;
 using Clinic.Core.Entities;
 using ClinicApi.Models.Notification;
-using System;
 
 namespace ClinicApi.Automapper
 {
@@ -11,17 +10,24 @@ namespace ClinicApi.Automapper
         public NotificationProfile()
         {
             CreateMap<NotificationDto, NotificationModel>()
-                .ForMember(n => n.Message, options => options.MapFrom(nDto => nDto.Content));
+                .ForMember(n => n.Message, options => options.MapFrom(nDto => nDto.Content))
+                .ForMember(n => n.CreationDate, options => options.MapFrom(nDto => nDto.CreatedDate));
             CreateMap<NotificationModel, NotificationDto>()
-                .ForMember(n => n.Content, options => options.MapFrom(nModel => nModel.Message));
+                .ForMember(n => n.Content, options => options.MapFrom(nModel => nModel.Message))
+                .ForMember(n => n.CreatedDate, options => options.MapFrom(nModel => nModel.CreationDate));
 
             CreateMap<CreateNotificationModel, CreateNotificationDto>()
                 .ForMember(nDto => nDto.AuthorId, options => options.Ignore())
-                .ForMember(nDto => nDto.IsRead, options => options.Ignore());
+                .ForMember(nDto => nDto.IsRead, options => options.Ignore())
+                .ForMember(nDto => nDto.CreationDate, options => options.MapFrom(nModel => nModel.CreationDate));
+            CreateMap<CreateNotificationDto, CreateNotificationModel>()
+                .ForMember(nModel => nModel.CreationDate, options => options.MapFrom(nDto => nDto.CreationDate));
 
             CreateMap<Notification, NotificationModel>()
                 .ForMember(nModel => nModel.Message, options => options.MapFrom(n => n.Content))
-                .ForMember(nModel => nModel.CreationDate, options => options.MapFrom(n => (DateTimeOffset)n.CreationDate));
+                .ForMember(nModel => nModel.CreationDate, options => options.MapFrom(n => n.CreationDate));
+            CreateMap<NotificationModel, Notification>()
+                .ForMember(n => n.CreationDate, options => options.MapFrom(nModel => nModel.CreationDate));
 
             CreateMap<UpdateNotificationModel, Notification>()
                 .ForMember(n => n.Author, options => options.Ignore())
